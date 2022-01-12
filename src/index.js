213a0073e4e15ae73e6f6b9c92b4026d201a2f5e
Feiyu Wong <feiyucw2@illinois.edu>
2022-01-11 23:51:23 -0600

    fixed safari compatability issues in terms of smooth scroll and font size calculations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import smoothscroll from 'smoothscroll-polyfill';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+smoothscroll.polyfill();
 
 ReactDOM.render(
   <React.StrictMode>
